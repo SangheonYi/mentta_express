60b080d42481b46b48b8468513d146ee0b2c9657
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-11-21 17:09:46 +0900

    fix: Fix request body

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var axios = require("axios");
 var express = require("express");
 var app = express();
-
-// const host = "https://d32b76b5f9314b9d9e92343ce542ab46.apigw.ntruss.com";
 const url =
   "https://d32b76b5f9314b9d9e92343ce542ab46.apigw.ntruss.com/custom/v1/5837/b0b1a01bfe8a0beb316ee90289721eeb5b2c43fbb5321b45cee82c40a3f6fe12";
 const secretKey = "Vm5Vakxzd3FTQkFWQnlMeUZXdW9WSXptbk1VWXpQT08=";
@@ -19,29 +17,33 @@ function makeSign(body) {
 app.get("/", function (req, res) {
   res.send("<h1>hi friend!</h1>");
   const requestBody = {
+    version: "v2",
+    userId: "U47b00b58c90f8e47428af8b7bddcda3d",
+    userIp: "8.8.8.8",
     timestamp: 12345678,
     bubbles: [
       {
         type: "text",
-        data: { description: "postback text of welcome action" },
+        data: { description: "r u mentta?" },
       },
     ],
     event: "send",
   };
-  const stringed = JSON.stringify(requestBody);
-  const encrypted = makeSign(stringed);
+  const jsonbody = JSON.stringify(requestBody);
+  const encrypted = makeSign(jsonbody);
+  console.log(`💥`);
   console.log(encrypted);
-
   axios
-    .post(url, {
+    .post(url, requestBody, {
       headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=utf-8",
+        "Content-type": "application/json;UTF-8",
         "X-NCP-CHATBOT_SIGNATURE": encrypted,
       },
-      requestBody,
     })
     .then((response) => {
-      console.log(response.data);
+      const { data } = response;
+      console.log(data);
+      console.log(data.bubbles[0]);
       return response;
     })
     .catch((error) => {
