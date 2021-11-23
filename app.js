662526e13761e88bbcb79b97d98a9dff3d07547d
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-11-23 08:31:20 +0000

    feat: Feat clova chatbot api server

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var axios = require("axios");
 var express = require("express");
+var cors = require("cors");
 var app = express();
+var router = express.Router();
 const url =
   "https://d32b76b5f9314b9d9e92343ce542ab46.apigw.ntruss.com/custom/v1/5837/b0b1a01bfe8a0beb316ee90289721eeb5b2c43fbb5321b45cee82c40a3f6fe12";
 const secretKey = "Vm5Vakxzd3FTQkFWQnlMeUZXdW9WSXptbk1VWXpQT08=";
-const portnum = 8000;
+const portnum = 8081;
 app.listen(portnum, function () {});
 
 function makeSign(body) {
@@ -14,8 +16,9 @@ function makeSign(body) {
   return signatureHeader;
 }
 
-app.get("/", function (req, res) {
-  res.send("<h1>hi friend!</h1>");
+app.use(cors());
+app.use("/clovaChat", router)
+router.get("/", cors(), async function(req, res){
   const requestBody = {
     version: "v2",
     userId: "U47b00b58c90f8e47428af8b7bddcda3d",
@@ -33,7 +36,7 @@ app.get("/", function (req, res) {
   const encrypted = makeSign(jsonbody);
   console.log(`💥`);
   console.log(encrypted);
-  axios
+  const chat = await axios
     .post(url, requestBody, {
       headers: {
         "Content-type": "application/json;UTF-8",
@@ -42,12 +45,19 @@ app.get("/", function (req, res) {
     })
     .then((response) => {
       const { data } = response;
-      console.log(data);
       console.log(data.bubbles[0]);
-      return response;
+      return data.bubbles[0].data.description;
     })
     .catch((error) => {
       console.log(error);
       return error;
     });
+    console.log(chat);
+    res.json({chat})
 });
+
+app.get("/",  function (req, res) {
+
+  return 'this is 💜 root return';
+});
+
