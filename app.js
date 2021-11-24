9696ad9b2f17a9b0adde886c07f5fb06a6d0a1f0
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-11-24 05:33:21 +0000

    feat: Feat npc meta data

--- app.js
+++ app.js
@@ -7,18 +7,102 @@ const url =
   "https://d32b76b5f9314b9d9e92343ce542ab46.apigw.ntruss.com/custom/v1/5837/b0b1a01bfe8a0beb316ee90289721eeb5b2c43fbb5321b45cee82c40a3f6fe12";
 const secretKey = "Vm5Vakxzd3FTQkFWQnlMeUZXdW9WSXptbk1VWXpQT08=";
 const portnum = 8081;
+var domainInfo = {
+  "guard": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "king": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "villagegirl": {
+      url,
+      privateKey:secretKey,
+  },
+  "villager": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "agent": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "rick": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "scientist": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "nyan": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "beachnpc": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "forestnpc": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "desertnpc": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "lavanpc": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "priest": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "sorcerer": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "octocat": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "coder": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "beachnpc": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "desertnpc": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  },
+  "othernpc": {
+      url:"http://localhost:8081/clovaChat",
+      privateKey:"",
+  }
+};
+app.use(express.json())
 app.listen(portnum, function () {});
 
-function makeSign(body) {
+function makeSign(body, key) {
   const HmacSHA256 = require("crypto-js/hmac-sha256");
   const EncBase64 = require("crypto-js/enc-base64");
-  signatureHeader = HmacSHA256(body, secretKey).toString(EncBase64);
+  signatureHeader = HmacSHA256(body, key).toString(EncBase64);
   return signatureHeader;
 }
 
 app.use(cors());
 app.use("/clovaChat", router)
-router.get("/", cors(), async function(req, res){
+router.post("/", cors(), async function(req, res){
+  const { description, npc } = req.body;
+  // 이거 쪼개서 url, secretkey에 넣자
+  const npcDomain = domainInfo[npc]
+  console.log(npcDomain);
+  const eventValue = description ? 'send' : 'open';
   const requestBody = {
     version: "v2",
     userId: "U47b00b58c90f8e47428af8b7bddcda3d",
@@ -27,13 +111,13 @@ router.get("/", cors(), async function(req, res){
     bubbles: [
       {
         type: "text",
-        data: { description: "r u mentta?" },
+        data: { description },
       },
     ],
-    event: "send",
+    event: eventValue,
   };
   const jsonbody = JSON.stringify(requestBody);
-  const encrypted = makeSign(jsonbody);
+  const encrypted = makeSign(jsonbody, secretKey);
   console.log(`💥`);
   console.log(encrypted);
   const chat = await axios
@@ -45,7 +129,10 @@ router.get("/", cors(), async function(req, res){
     })
     .then((response) => {
       const { data } = response;
-      console.log(data.bubbles[0]);
+      if (data)
+        console.log('data: ', data);
+      else
+        console.log('🚾')
       return data.bubbles[0].data.description;
     })
     .catch((error) => {
@@ -57,7 +144,6 @@ router.get("/", cors(), async function(req, res){
 });
 
 app.get("/",  function (req, res) {
-
   return 'this is 💜 root return';
 });
 
